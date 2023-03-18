f2c297e4ed09fe284f6b90a8f5a153453aaf8b1c
amanoizumi <a0918233246@gmail.com>
2023-03-18 19:41:44 +0800

    add: 貼文刪除、編輯功能

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,25 +1,19 @@
-const Posts = require('../models/PostsModel.js');
+const Post = require('../models/PostsModel.js');
 const handleSuccess = require('../service/handleSuccess');
 const handleError = require('../service/handleError');
 
 const posts = {
-  async getPosts({ req, res }) {
-    const allPosts = await Posts.find();
+  async getPosts(req, res) {
+    const allPosts = await Post.find();
     handleSuccess(res, allPosts);
   },
 
-  async createPosts({ req, res }) {
+  async createPosts(req, res) {
     try {
-      const { body } = req;
-      console.log(req);
-      if (body.content) {
-        const newPost = await Posts.create({
-          name: body.name,
-          content: body.content,
-          tags: body.tags,
-          type: body.type,
-        });
-        handleSuccess(res, newPost);
+      const data = req.body;
+      if (data.name && data.content) {
+        const info = await Post.create(data);
+        handleSuccess(res, info);
       } else {
         handleError(res);
       }
@@ -27,34 +21,41 @@ const posts = {
       handleError(res, err);
     }
   },
-  // async deleteAllPosts({ req, res }) {
-  //   const posts = await Posts.deleteMany({});
-  //   handleSuccess(res, posts);
-  // },
-  // async deleteOnePost({ id, req, res }) {
-  //   try {
-  //     await Posts.findByIdAndDelete(id);
-  //     handleSuccess(res, null);
-  //   } catch (err) {
-  //     handleError(res, err);
-  //   }
-  // },
-  // async editOnePost({ id, body, req, res }) {
-  //   try {
-  //     const data = JSON.parse(body);
-  //     if (data.content !== undefined) {
-  //       const editContent = {
-  //         content: data.content,
-  //       };
-  //       const editPost = await Posts.findByIdAndUpdate(id, editContent);
-  //       handleSuccess(res, editPost);
-  //     } else {
-  //       handleError(res);
-  //     }
-  //   } catch (err) {
-  //     handleError(res, err);
-  //   }
-  // },
+  async deleteAllPosts(req, res) {
+    const posts = await Post.deleteMany({});
+    handleSuccess(res, posts);
+  },
+  async deletePostByID(req, res) {
+    try {
+      const { id } = req.params;
+      const info = await Post.findByIdAndDelete(id);
+      if (info === null) {
+        handleError(res);
+      } else {
+        handleSuccess(res);
+      }
+    } catch (err) {
+      handleError(res, err);
+    }
+  },
+  async editOnePost(req, res) {
+    try {
+      const { id } = req.params;
+      const { body } = req;
+
+      if (body.content !== undefined) {
+        const editContent = {
+          content: body.content,
+        };
+        const editPost = await Post.findByIdAndUpdate(id, editContent);
+        handleSuccess(res, editPost);
+      } else {
+        handleError(res);
+      }
+    } catch (err) {
+      handleError(res, err);
+    }
+  },
 };
 
 module.exports = posts;
--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,20 @@ const express = require('express');
 const router = express.Router();
 const PostControllers = require('../controllers/posts');
 
-router.get('/', PostControllers.getPosts);
-router.post('/', PostControllers.createPosts);
+router.get('/', (req, res) => {
+  PostControllers.getPosts(req, res);
+});
+router.post('/', (req, res) => {
+  PostControllers.createPosts(req, res);
+});
+router.delete('/', (req, res) => {
+  PostControllers.deleteAllPosts(req, res);
+});
+router.delete('/:id', (req, res) => {
+  PostControllers.deletePostByID(req, res);
+});
+router.patch('/:id', (req, res) => {
+  PostControllers.editOnePost(req, res);
+});
 
 module.exports = router;
